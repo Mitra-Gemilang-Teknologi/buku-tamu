78160ac188006f8ef6c26cd704a5cf6356e1bfa4
Dani Hidayat <danihidayat015@gmail.com>
2021-12-04 23:26:20 +0700

    update title

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     {{-- my style --}}
     <link rel="stylesheet" href="/css/style.css">
-    <title>Urgent |{{$title}}</title>
+    <title>Kzn |{{$title}}</title>
   </head>
   <body>
 @include('partials.navbar')
