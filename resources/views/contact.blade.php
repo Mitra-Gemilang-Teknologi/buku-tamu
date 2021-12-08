bf974ccd758508bd4671f2853d2f9f7475eda83f
Dani Hidayat <danihidayat015@gmail.com>
2021-12-08 16:45:23 +0700

    update footer contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -65,14 +65,7 @@
       </div>
     </div>
   </main>
-
-  <footer class="my-5 pt-5 text-muted text-center text-small">
-    <p class="mb-1">&copy; {{ date('Y') }} Kelas Zaman Now</p>
-    <ul class="list-inline">
-      <li class="list-inline-item"><a href="#">Privacy</a></li>
-      <li class="list-inline-item"><a href="#">Terms</a></li>
-      <li class="list-inline-item"><a href="#">Support</a></li>
-    </ul>
-  </footer>
+  @include('partials.footer')
+  
 </div>
     @endsection
