476184318d07e55dba336a137e6e58668d647664
Dani Hidayat <46246911+DaniHidayat@users.noreply.github.com>
2023-01-25 13:16:32 +0700

    update

--- resources/views/dashboard/bukutamu/index.blade.php
+++ resources/views/dashboard/bukutamu/index.blade.php
@@ -109,7 +109,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Laporan </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -132,8 +132,8 @@
                         </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
 							</form>
 
