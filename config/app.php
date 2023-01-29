e01a241c38917e95956be048a6629abed057547a
Dani Hidayat <46246911+DaniHidayat@users.noreply.github.com>
2023-01-29 16:50:50 +0700

    add

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
 		Illuminate\View\ViewServiceProvider::class,
 		RealRashid\SweetAlert\SweetAlertServiceProvider::class,
 		Maatwebsite\Excel\ExcelServiceProvider::class,
+		Spatie\Permission\PermissionServiceProvider::class,
 
 		/*
          * Package Service Providers...
