6532c52a6b6e390b168e258fffc984fb55bab097
Dani Hidayat <46246911+DaniHidayat@users.noreply.github.com>
2023-02-23 09:27:10 +0700

    update service

--- database/seeders/SubServiceTypeSeeder.php
+++ database/seeders/SubServiceTypeSeeder.php
@@ -81,162 +81,162 @@ class SubServiceTypeSeeder extends Seeder
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Tenaga Penyuluh Kesehatan Masyarakat'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Tenaga Sanitasi Kesehatan Lingkungan'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik dan Izin Kerja Tenaga Gizi'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik dan Izin Kerja Tenaga Keterapian Fisik'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik dan Izin Kerja Tenaga Teknik Biomedika'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Tenaga Kesehatan Tradisional Ramuan'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Radiolografer dan Izin Radiologi'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Laboratorium Klinik'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Refraksionis dan Izin Operasional Optikal'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Penyelenggaraan Pelayanan Hermodialisa'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Laboratorium Klinik'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Unit Transfusi Darah'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Apotik'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Fasilitas Pelayanan Kesehatan Tradisional'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Panti Sehat'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Terapis Gigi dan Mulut'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Tukang Gigi'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Laboratorium Kesehatan Medis Dasar'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Ahli Teknologi Laboratorium Kesehatan'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Laboratorium Milik Pemerintah'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Pelayanan Skincare'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Pijat Untuk Kesehatan'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Klinik Fisioterapi'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Tabib Patah Tulang'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Terapis Wicara'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Tabib Akupuntur'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik elektromedis'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Klinik Radiografer'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Klinik Radiografis'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Penata Anestesi'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Tekhnik Kardiovaskuler'
 		]);
 
 		SubServiceType::create([
-			'id_service_type' => 2,
+			'id_service_type' => 4,
 			'sub_service_name' => 'Izin Praktik Perekam Medis dan Informasi Kesehatan'
 		]);
 
--- resources/views/bukuTamu/index.blade.php
+++ resources/views/bukuTamu/index.blade.php
@@ -146,6 +146,8 @@
                                 </div>
                             </div>
 
+
+
                                 <div class="step">
 
                                 <div class="row mb-3">
@@ -163,6 +165,7 @@
 
 
                            		 </div>
+
 
                                 <div class="step">
                                 <div class="row mb-3" style="align-items: end !important;">
@@ -374,44 +377,20 @@
 
         <!-- <a href="#0" class="cd-nav-trigger">Menu<span class="cd-icon"></span></a> -->
         <!-- /menu button -->
-
-        <!-- Modal terms -->
-        <div class="modal fade" id="terms-txt" tabindex="-1" role="dialog" aria-labelledby="termsLabel"
-            aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title" id="termsLabel">Terms and conditions</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Lorem ipsum dolor sit amet, in porro albucius qui, in <strong>nec quod novum accumsan</strong>,
-                            mei ludus tamquam dolores id. No sit debitis meliore postulant, per ex prompta alterum sanctus,
-                            pro ne quod dicunt sensibus.</p>
-                        <p>Lorem ipsum dolor sit amet, in porro albucius qui, in nec quod novum accumsan, mei ludus tamquam
-                            dolores id. No sit debitis meliore postulant, per ex prompta alterum sanctus, pro ne quod dicunt
-                            sensibus. Lorem ipsum dolor sit amet, <strong>in porro albucius qui</strong>, in nec quod novum
-                            accumsan, mei ludus tamquam dolores id. No sit debitis meliore postulant, per ex prompta alterum
-                            sanctus, pro ne quod dicunt sensibus.</p>
-                        <p>Lorem ipsum dolor sit amet, in porro albucius qui, in nec quod novum accumsan, mei ludus tamquam
-                            dolores id. No sit debitis meliore postulant, per ex prompta alterum sanctus, pro ne quod dicunt
-                            sensibus.</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn_1" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-                <!-- /.modal-content -->
-            </div>
-            <!-- /.modal-dialog -->
-        </div>
-        <!-- /.modal -->
     </div>
 @endsection
 @push('scripts')
     <script>
+
         //Buat Animasi
         $(document).ready(function() {
+
+					var elements = document.querySelectorAll('.step');
+					var values = [].map.call(elements, function(e) {
+						return e.html;
+						values[1].classList.remove("step");
+					});
+					console.log('values',values[1]);
 
             $("html,body").animate({
                 scrollTop: 0
