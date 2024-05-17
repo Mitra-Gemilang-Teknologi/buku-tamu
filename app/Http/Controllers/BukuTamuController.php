cbc92a15285907cf235dcfaa9d1dd7658e4c3c9f
DaniHidayat <danihidayat015@gmail.com>
2024-05-17 08:35:19 +0700

    update desc

--- app/Http/Controllers/BukuTamuController.php
+++ app/Http/Controllers/BukuTamuController.php
@@ -65,7 +65,7 @@ class BukuTamuController extends Controller
 
 
 		return view('ListTamu.index', [
-			'Visit' => Visit::with('village')->with('district')->get(),
+			'Visit' => Visit::with(['village', 'district'])->orderBy('created_at', 'DESC')->get(),
 			"title" => "List Buku Tamu",
 			"active" => "List Buku Tamu",
 		]);
